ae6e74b8f54ce6fc097fbccd62de8f80729cab48
Micah Bowie <micahbowie20@gmail.com>
2020-06-29 01:34:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,9 +4,3 @@ require 'bigdecimal/math'
 # calculate pi to 10k digits
 puts Benchmark.measure { BigMath.PI(10_000) }
   def prime?(num)
-    return false if num <=1
-    (2..(num - 1)).each do |n|
-      return false if num % n == 0
-    end
-    true
-  end
